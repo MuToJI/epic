f07e4487efc017a48f83062ebf1284ba60b20fcd
Alex K <fabian91alex@mail.ru>
2016-05-14 12:32:05 +0300

    addindexphp

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+//file_put_contents('test.txt', 'string', FILE_APPEND);
+
+
+//echo file_get_contents('test.txt');
+
+//$file = fopen('test.txt', 'r');
+//while (($str = fgets($file)) !== false){
+  //  echo $str;
+//} 
+//fclose('$file');
+
+//fputs($file, "test\r\n");
+
+//fclose('$file');
+
+//$file = glob('*.php');
+
+//var_dump('$file');
+
+//$file = glob('*');
+//file_get_contents ('*');
+
+//$files = glob('C:\*');
+//foreach ($files as $file) {
+  //  var_dump($file);
+//}
+
+// $array = ['a' =1, 'b' = 2, 'c' = 3];
+//  foreach ($array as $key => $val) {
+//    echo "$key => $val<br/>";
+//  }
+
+$files = glob('C:\*');
+foreach ($files as $file) {
+    echo htmlentities(file_get_contents($file)) . '<br>';
+    echo '<hr/>';
+}
+
